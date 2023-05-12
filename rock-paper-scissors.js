c779fdf6a7b71253d722dbc2a2d02a16e4809c5d
Hope G <119355967+Ho0pHo0p@users.noreply.github.com>
2023-05-12 16:42:44 -0400

    winner displayed

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,37 +7,93 @@ let scissorsElement = document.querySelector(
   '.js-button-scissors');
 let clearElement = document.querySelector('.js-button-clear');
 
-let userInput = '';
-let compInput = '';
+let userInput ='';
+let compInput = computerChoice();
 
 rockElement.addEventListener('click',()=>{
   let userInput = 'rock'
+  console.log(userInput);
+  choices();
+  results();
 });
 paperElement.addEventListener('click',()=>{
   let userInput = 'paper'
+  console.log(userInput);
+  computerChoice();
+  choices();
+  results();
 });
 scissorsElement.addEventListener('click',()=>{
   let userInput = 'scissors'
+  console.log(userInput);
+  computerChoice();
+  choices();
+  results();
 });
 clearElement.addEventListener('click',()=>{
 
 });
 
 
+function choices() {
+  
+  document.getElementById('user-input').innerHTML = `you chose ${userInput}`;
 
-function results() {}
+  document.getElementById('comp-input').innerHTML = `poop chose ${compInput}`;
+}
+
+function results() {
+  if (compInput === userInput) {
+    document.getElementById('results').innerHTML = `it's a tie!`;
+  } 
+  else if (compInput !== userInput){
+    compare();
+  }
+}
+
+function compare () {
+  if (userInput === 'rock'){
+    if (compInput === 'scissors'){
+      document.getElementById('results').innerHTML = `you win!`;
+    }
+    else if (compInput === 'paper'){
+      document.getElementById('results').innerHTML = `you lose`;
+    }
+  }
+
+  if (userInput === 'paper'){
+    if (compInput === 'rock'){
+      document.getElementById('results').innerHTML = `you win!`;
+    }
+    else if (compInput === 'scissors'){
+      document.getElementById('results').innerHTML = `you lose`;
+    }
+  }
+
+  if (userInput === 'scissors'){
+    if (compInput === 'paper'){
+      document.getElementById('results').innerHTML = `you win!`;
+    }
+    else if (compInput === 'rock'){
+      document.getElementById('results').innerHTML = `you lose`;
+    }
+  }
+
+
+  }
 
 function computerChoice() {
   let randomNum = Math.floor(Math.random() * 3); 
 
   if (randomNum == 0) {
-    let compInput = 'rock';
+    return 'rock'
   }
   else if (randomNum == 1){
-    let compInput = 'paper';
+    return'paper';
   }
   else if (randomNum == 2){
-    let compInput = 'scissors';
+    return 'scissors';
   }
+
 }
 
